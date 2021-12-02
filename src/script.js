844dd8a35a5362b221c3e7df0329ad1b04f3f6e1
Almudena <almudenaperezvalencia@MacBook-Air-de-Almudena.local>
2021-12-02 13:40:30 +0100

    cambios tutoria marcos

--- src/script.js
+++ src/script.js
@@ -11,18 +11,36 @@ window.onload = () => {
 
     // Pintamos Pacman
     let actors = [
+        map,
         new Pacman({ x: 200, y: 100 }, "yellow", 10),
         new Pacman({ x: 200, y: 200 }, "pink", 12),
         new Pacman({ x: 200, y: 300 }, "blue", 15),
         new Pacman({ x: 200, y: 400 }, "grey", 5),
+        
     ];
 
     // GAME LOOP --> Bucle de renderizado y actualización
-    setInterval(() => {
-        actors.forEach(e => e.update());
-        ctx.clearRect(0, 0, 500, 500);
-        actors.forEach(e => { e.draw(ctx) });
-    }, 100)
+   // setInterval(() => {
+       // actors.forEach(e => e.update());
+       // ctx.clearRect(0, 0, 500, 500);
+       // actors.forEach(e => { e.draw(ctx) });
+   // }, 100)
+
+   let lastFrame = 0;
+   const render = (time) => {
+     // console.log(time);
+     let delta = (time - lastFrame) / 1000;
+     // console.log(delta);
+     lastFrame = time;
+     actors.forEach((e) => e.update(delta));
+     ctx.clearRect(0, 0, canvas.width, canvas.height);
+     actors.forEach((e) => {
+       e.draw(delta, ctx);
+     });
+     window.requestAnimationFrame(render);
+   };
+ 
+   window.requestAnimationFrame(render);
 
     document.body.addEventListener("keydown", (e) => {
         // console.log(e.key);
